402d93dfd0b397dc3d8b3d4e5cecb9ce23e801e3
Consiglia D'Amaro <consigliadamaro0@gmail.com>
2022-04-05 15:12:12 +0200

    feat: modified exercise 11-12 and added exercise 13

--- Esercizio13/exercise-3.js
+++ Esercizio13/exercise-3.js
@@ -1,22 +1,24 @@
-const sum = (a, b) => {
+const sum = (a, b) =>  a+b;
+/* {
   return a + b;
-}
+} */
 
-const subtract = (a, b) => {
+const subtract = (a, b) => a-b; 
+/* {
   return a - b;
-}
+} */
 
-const multiply = (a, b) => {
+const multiply = (a, b) => a*b;
+/* {
   return a * b;
-}
+} */
 
-const divide = (a, b) => {
+const divide = (a, b) => a/b;
+/* {
   return a / b;
-}
+} */
 
-const log = (value) => {
-  console.log(value);
-}
+const log = (value) => console.log(value);
+log(divide(subtract(multiply(sum(2,4), sum(5,2)),2),5));
 
 
-/* `((2 + 4) * (5 + 2) - 2) / 5` */
