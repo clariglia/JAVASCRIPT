0e7d3ef657f65feba49405e35cf594d740c5a7c6
Consiglia D'Amaro <consigliadamaro0@gmail.com>
2022-04-07 19:49:24 +0200

    feat: added exercises 25-26

--- Esercizio25/exercise-1.js
+++ Esercizio25/exercise-1.js
@@ -1,6 +1,29 @@
 const person = {
-  // ...
+  firstName: '',
+  lastName: '',
+  get nome(){
+    return this.firstName;
+  },
+  set nome(value){
+    return (this.firstName = value);
+  },
+  get cognome(){ 
+    return this.lastName;
+  },
+  set cognome(value){
+    return (this.lastName = value);
+  },
+   fullName(){
+    return (`${this.firstName} ${this.lastName}`);
+  },
+  
 }
 
+const john = Object.create(person)
+const simon = Object.create(person)
+john.nome = 'John'
+john.cognome = 'Doe'
+simon.nome = 'Simon'
+simon.cognome = 'Collins'
 console.log(john.fullName()); // John Doe
 console.log(simon.fullName()); // Simon Collins
