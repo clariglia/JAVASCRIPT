f4e7f1a261f9d45bd824ed0e5536c45ccdf74148
Consiglia D'Amaro <consigliadamaro0@gmail.com>
2022-05-02 20:37:05 +0200

    feat: exercise 56

--- Esercizio56/exercise-4.js
+++ Esercizio56/exercise-4.js
@@ -1,6 +1,6 @@
 function sum(...array) {
   /* return num1 + num2 + num3; */
-  return array = array.reduce((sum) => sum, 0)
+  return array.reduce((sum, value) => sum + value)
 }
 
 /* const numbers = [1, 2, 3]; */
